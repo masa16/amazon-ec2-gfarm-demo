efe0b207d80e101fc833ab9f924345ccc58a85e1
Masahiro TANAKA <masa16.tanaka@gmail.com>
2015-08-31 19:21:50 +0900

    minor fix

--- prepare-demo.sh
+++ prepare-demo.sh
@@ -1,9 +1,10 @@
 source ./env.sh
 
 rcmd='
-mkdir gfm
-gfarm2fs gfm
-cd gfm
+d=gfm
+mkdir -p $d
+gfarm2fs $d
+cd $d
 wget -nv https://s3-us-west-2.amazonaws.com/masa16/pwrake/montage-m31.tar -O - | tar xf -
 cd montage-m31
 cat <<EOF > hosts
